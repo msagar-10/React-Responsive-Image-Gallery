72d5a1bbd73036079d90fb1f30d486fec87c3de5
msagar-10 <mhossainsagar@gmail.com>
2023-11-06 10:00:38 +0600

    add:main

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
     const updatedCards = cards.filter((card) => !card.checked);
     setCards(updatedCards);
   };
-  
+
   const selectedCount = cards.filter((card) => card.checked).length;
 
   //  Image Upload Function
@@ -128,6 +128,9 @@ const App = () => {
                   onDragOver={(e) => handleDragOver(e, card)}
                   onDragStart={(e) => handleDragStart(e, card)}
                   onDragEnd={handleDragEnd}
+                  onTouchStart={(e) => handleDragStart(e, card)}
+                  onTouchMove={(e) => handleDragOver(e, card)}
+                  onTouchEnd={handleDragEnd}
                   draggable
                 >
                   <img src={card.img} alt="#" />
